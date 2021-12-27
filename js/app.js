35dfdbc86a04ecda6b2c01926b2fefbb7e78ac71
Anuar Kairulla <anuarkairulla@gmail.com>
2021-12-27 19:56:41 +0600

    update 27.12

--- js/app.js
+++ js/app.js
@@ -2,21 +2,13 @@ $(function () {
     let $ = jQuery;
     let btn = document.querySelector('.checkboxBtn');
     let dataPlace = document.querySelector('#data-placement');
-    btn.disabled = true;
-    let allExpandedParent = [];
-    let expandedParent = [];
-    let allNotExpanded = [];
-    let notExpanded = [];
-    let parentRow = [];
-    let fields = [];
-    let rowData = {};
+    // btn.disabled = true;
     let crudServiceBaseUrl = 'https://demos.telerik.com/kendo-ui/service';
-
     let dataSource = new kendo.data.TreeListDataSource({
         transport: {
             read: {
-                url: crudServiceBaseUrl + '/EmployeeDirectory',
-                // url: crudServiceBaseUrl + '/EmployeeDirectory/All',
+                // url: crudServiceBaseUrl + '/EmployeeDirectory',
+                url: crudServiceBaseUrl + '/EmployeeDirectory/All',
                 dataType: 'jsonp'
             }
         },
@@ -32,151 +24,161 @@ $(function () {
             },
         }
     });
+    let columns = [
+        { field: 'FirstName', title: 'First Name' }
+    ]
 
-    let buildTreeList = () => {
-        $('#treelist').kendoTreeList({
+    // let dsLoop = function (data, func) {
+    //     for (let i = 0; i < data.length; i++) {
+    //         func(data)
+    //     }
+    // }
+
+    dataSource.read().then((e) => {
+        let data = dataSource.data();
+        let expandedRows = [];
+        let fields = [];
+        let isModfied;
+        for (let item of columns) {
+            fields.push(item.field);
+        };
+        // data[0].FirstName = 'Сын'
+        $("#treelist").kendoTreeList({
             dataSource: dataSource,
-            columns: [
-                { field: 'FirstName', expandable: true, title: 'First Name', width: 250 },
-                { field: 'LastName', title: 'Last Name' },
-                { field: 'Position' },
-                { field: 'Extension', title: 'Ext', format: '{0:#}' }
-            ],
-            dataBound: function (e) {
-                btn.disabled = false;
-                let DS = e.sender.dataSource.data()
-                dataPlace.textContent = js_beautify(JSON.stringify(DS.toJSON()));
-                for (let i = 0; i < DS.length; i++) {
-                    let row = DS[i]
-                    if (row.hasChildren) { allExpandedParent.push(row) } else { allNotExpanded.push(row) }
-                }
-                notExpanded = [];
-                for (let i in e.sender.columns) {
-                    let item = e.sender.columns[i];
-                    if (fields.length <= i) {
-                        fields.push({ id: i, field: item.field, title: item.title || item.field });
-                    }
-                };
-                for (let i = 0; i < DS.length; i++) {
-                    let data = DS[i];
-                    if (!data.expanded) { notExpanded.push(data); };
-                };
-            },
-            expand: function (e) {
-                // notExpanded = [];
-                let expTarget = e.model._loaded;
-                for (let i in e.sender.columns) {
-                    let item = e.sender.columns[i];
-                    if (fields.length <= i) {
-                        fields.push({ id: i, field: item.field, title: item.title || item.field });
-                    }
-                };
-                for (let i = 0; i < e.sender.dataSource.data().length; i++) {
-                    let data = e.sender.dataSource.data()[i];
-                }
-                if (!expTarget) { expandedParent.push(e.model) };
-            }
-        });
-    };
-
-    buildTreeList();
-
-    btn.addEventListener('click', (e) => {
-        document.querySelector('#treelist').innerHTML = '';
-        document.querySelector('#treelist').style.width = '';
-        let columns = [];
-        let levels = [];
-        let expandedRowIds = [];
-        let nonExpParentId;
-        let newDS = dataSource.data();
-        if (expandedParent.length === 0 && notExpanded.length === 0) {
-            expandedParent = allExpandedParent;
-            notExpanded = allNotExpanded;
-        }
-        allExpandedParent = [];
-        allNotExpanded = [];
-        if (expandedParent.length === 0 && notExpanded.length > 0) {
-            for (let row of notExpanded) {
-                if (row.hasChildren) {
-                    allExpandedParent.push(row)
-                } else {
-                    allNotExpanded.push(row)
-                }
-            }
-            expandedParent = allExpandedParent;
-            allNotExpanded = allNotExpanded;
-        }
-
-        if (newDS.length === 1) {
-            expandedParent = [];
-        }
-        for (let item of expandedParent) {
-            expandedRowIds.push(item.id);
-        };
-        for (let i = 0; i < newDS.length; i++) {
-            let data = newDS[i];
-            for (let id of expandedRowIds) {
-                if (data.id === id) {
-                    rowData[id] = data;
-                    rowData.length = id;
-                };
-            };
-            for (let item of fields) {
-                let level = dataSource.level(data);
-                data[`${item.field + level}`] = data[item.field];
-            };
-            levels.push(dataSource.level(data));
-        };
-        levels = [...new Set(levels)];
-        for (let item of fields) {
-            for (let i in levels) {
-                columns.push(
-                    { field: item.field + i, title: `${item.title} ${Number(i) + 1}` }
-                )
-            }
-        };
-        for (let nonExpRow of notExpanded) {
-            for (let expRow of expandedParent) {
-                if (nonExpRow.parentId === expRow.id) {
-                    for (let item of fields) {
-                        nonExpRow[`${item.field + dataSource.level(expRow)}`] = expRow[item.field];
-                    };
-                    nonExpParentId = nonExpRow.parentId;
-                    while (nonExpParentId) {
-                        for (let i = 0; i < newDS.length; i++) {
-                            let data = newDS[i]
-                            if (data.id == nonExpParentId) {
-                                for (let item of fields) {
-                                    nonExpRow[`${item.field + dataSource.level(data)}`] = data[item.field];
-                                };
-                                nonExpParentId = data.parentId;
-                            }
+            columns: columns,
+            dataBound: (e) => {
+                var grid = e.sender;
+                let dataSource = e.sender.dataSource;
+                let data = dataSource.data();
+                let notExpandedRows = [];
+                let levels = [];
+                dataPlace.textContent = js_beautify(JSON.stringify(data.toJSON()));
+                if (expandedRows.length === 0) {
+                    notExpandedRows = [];
+                    for (let i = 0; i < data.length; i++) {
+                        let row = data[i];
+                        if (row.hasChildren) {
+                            expandedRows.push(row)
+                        } else {
+                            notExpandedRows.push(row)
                         }
                     }
                 }
+                for (let i = 0; i < data.length; i++) {
+                    let evRow = data[i];
+                    if (!evRow.expanded) {
+                        notExpandedRows.push(evRow);
+                    };
+                    levels.push(dataSource.level(evRow));
+                };
+                levels = [...new Set(levels)]
+                for (let expandedRow of expandedRows) {
+                    for (let notExpandedRow of notExpandedRows) {
+                        if (notExpandedRow.parentId === expandedRow.id) {
+                            for (let field of fields) {
+                                notExpandedRow[field + dataSource.level(notExpandedRow)] = notExpandedRow[field];
+                                notExpandedRow[field + dataSource.level(expandedRow)] = expandedRow[field];
+                            };
+                            let notExpRowPID = notExpandedRow.parentId;
+                            while (notExpRowPID) {
+                                for (let i = 0; i < data.length; i++) {
+                                    let row = data[i];
+                                    if (notExpRowPID === row.id) {
+                                        for (let field of fields) {
+                                            notExpandedRow[field + dataSource.level(row)] = row[field];
+                                        };
+                                        notExpRowPID = row.parentId;
+                                    };
+                                };
+                            };
+                        };
+                    };
+                };
+                if (btn.checked) {
+                    // columns = [{ field: "FirstName" }, { field: "FirstName0" }]
+                    // e.sender.setOptions({ "columns": columns });
+                    grid.newColumns = [];
+                    for (let level of levels) {
+                        for (let item of grid.oldColumns) {
+                            let title = item.title || item.field;
+                            grid.newColumns.push({ field: `${item.field}${level}`, title: `${title} ${level}` })
+                        };
+                    };
+                    let setColumns = [...grid.oldColumns, ...grid.newColumns];
+                    for (let i = 0; i < data.length; i++) {
+                        let row = data[i];
+                        let item = grid.dataSource.get(row.id);
+                        let tr = $("[data-uid='" + item.uid + "']", grid.tbody);
+                        tr.css({ 'display': 'table-row' });
+                    }
+                    for (let expandedRow of expandedRows) {
+                        let item = grid.dataSource.get(expandedRow.id);
+                        let tr = $("[data-uid='" + item.uid + "']", grid.tbody);
+                        tr.css({ 'display': 'none' });
+                    };
+                    if (grid.columns.length !== setColumns.length) {
+                        grid.setOptions({ 'columns': setColumns });
+                    };
+
+                } else {
+                    for (let i = 0; i < data.length; i++) {
+                        let row = data[i];
+                        var htmlRow = e.sender.itemFor(row);
+                        $(htmlRow).css({ "display": "table-row" });
+                    };
+                };
+            },
+            expand: (e) => {
+                var grid = e.sender;
+                let dataSource = e.sender.dataSource;
+                let row = e.model;
+                let levels = [];
+                grid.oldColumns = grid.oldColumns || columns;
+                if (!row._loaded) {
+                    expandedRows.push(row);
+                }
+            },
+            collapse: (e) => {
+                let dataSource = e.sender.dataSource;
+                let data = dataSource.data();
+            },
+            change: (e) => {
+                let dataSource = e.sender.dataSource;
+                let data = dataSource.data();
             }
-        }
-        if (expandedParent.length === 0) {
-            columns = [
-                { field: 'FirstName', title: 'First Name', width: 250 },
-                { field: 'LastName', title: 'Last Name' },
-                { field: 'Position' },
-                { field: 'Extension', title: 'Ext', format: '{0:#}' }
-            ]
-        }
-        if (btn.checked) {
-            $('#treelist').kendoGrid({
-                dataSource: newDS,
-                columns: columns,
-                width: columns.length * 200
-            });
-            let grid = $("#treelist").data("kendoGrid");
-            for (let id of expandedRowIds) {
-                var rowUID = grid.dataSource.get(id).uid;
-                $("[data-uid='" + rowUID + "']", grid.tbody).css({ 'display': 'none' });
+        })
+        btn.addEventListener('change', (e) => {
+            let grid = kendo.widgetInstance($('#treelist'));
+            let dataSource = grid.dataSource;
+            let data = dataSource.data();
+            let columns = grid.columns;
+            let toggle = e.target;
+            grid.oldColumns = grid.oldColumns || grid.columns;
+            if (toggle.checked) {
+                // grid.setOptions({ "columns": columns });
+                grid.newColumns = [];
+                let levels = [];
+                for (let i = 0; i < data.length; i++) {
+                    let row = data[i];
+                    levels.push(dataSource.level(row));
+                };
+                levels = [...new Set(levels)]
+                for (let level of levels) {
+                    for (let item of grid.oldColumns) {
+                        let title = item.title || item.field;
+                        grid.newColumns.push({ field: `${item.field}${level}`, title: `${title} ${level}` })
+                    }
+                };
+                let setColumns = [...grid.oldColumns, ...grid.newColumns]
+                grid.setOptions({ columns: setColumns })
+            } else {
+                for (let i = 0; i < data.length; i++) {
+                    let row = data[i];
+                    var htmlRow = grid.itemFor(row);
+                    $(htmlRow).css({ "display": "table-row" });
+                };
+                grid.setOptions({ columns: grid.oldColumns })
             };
-        } else {
-            buildTreeList();
-        }
-    })
+        });
+    });
 });
